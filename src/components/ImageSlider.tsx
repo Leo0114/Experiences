8a0eb8c7d97b11c9c3f97e27944bff67ad8996f7
Leo0114 <leonel0614@gmail.com>
2023-08-13 16:13:07 -0600

    goSlider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -2,24 +2,24 @@ import { useState } from "preact/hooks";
 
 const slides = [
   {
-    url: "https://images.unsplash.com/photo-1690669249460-65a76daaf698?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
-    title: "Boda",
+    url: "https://res.cloudinary.com/diiabesnk/image/upload/v1691964387/ksbqcoq2jdixuphzgsqh.webp",
+    title: "Food ",
   },
   {
-    url: "https://images.unsplash.com/photo-1682685796063-d2604827f7b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    title: "Banner",
+    url: "https://res.cloudinary.com/diiabesnk/image/upload/v1691964387/jkxvcuu2d10crnykv8tr.webp",
+    title: "Food 2",
   },
   {
-    url: "https://images.unsplash.com/photo-1683009427037-c5afc2b8134d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw0NXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    title: "guardias",
+    url: "https://res.cloudinary.com/diiabesnk/image/upload/v1691964387/neuz5thmvahkqtk2d74m.webp",
+    title: "Food 3",
   },
   {
-    url: "https://images.unsplash.com/photo-1690789824921-6754195c97ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    title: "sayula",
+    url: "https://res.cloudinary.com/diiabesnk/image/upload/v1691964388/wofztxlplgjivft8g6jt.webp",
+    title: "Food 4",
   },
   {
-    url: "https://images.unsplash.com/photo-1690743300330-d190ad8f97dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3M3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    title: "Villa",
+    url: "https://res.cloudinary.com/diiabesnk/image/upload/v1691964387/xhnmofgo7yz9xsui6wug.webp",
+    title: "Food 5",
   },
 ];
 export default function ImageSlider() {
@@ -39,12 +39,14 @@ export default function ImageSlider() {
   };
 
   return (
-    <div className="max-w-[1400px]  container  h-[540px] w-full m-auto  px-4 relative group ">
+    <div className="max-w-[1400px]  h-[580px] w-full m-auto  px-4 py-16 relative  ">
       <div
+        loading="lazy"
+        alt={slides[currentIndex].title}
         className=" w-full h-full rounded-2xl bg-center bg-cover  transition-all  duration-500"
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
       ></div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%]  left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer">
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%]  left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer bg-slate-900/40">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -61,7 +63,7 @@ export default function ImageSlider() {
           />
         </svg>
       </div>
-      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%]  right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer ">
+      <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%]  right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer bg-slate-900/40">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
